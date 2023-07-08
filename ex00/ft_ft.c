998e88c8be69b6d7dc1f87b36b1f29dfd0cca717
Flavien Camoin <fcamoin@z2r4p4.42lyon.fr>
2023-07-08 14:22:28 +0200

    C01

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -6,7 +6,7 @@
 /*   By: fcamoin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 10:05:06 by fcamoin           #+#    #+#             */
-/*   Updated: 2023/07/08 11:08:59 by fcamoin          ###   ########.fr       */
+/*   Updated: 2023/07/08 14:21:56 by fcamoin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,4 @@
 void	ft_ft(int *nbr)
 {
 	*nbr=42;
-}
-
-int	main()
-{
-	int nombre;
-	ft_ft=&nombre;
-	printf(%d\n
-	ft_ft();
-	return (0);
 }

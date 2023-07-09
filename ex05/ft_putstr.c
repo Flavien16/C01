4a37efedd3cd4939a867a068730a1dae44fb01bb
Flavien Camoin <fcamoin@z1r3p2.42lyon.fr>
2023-07-09 15:32:27 +0200

    C01

--- ex05/ft_putstr.c
+++ ex05/ft_putstr.c
@@ -6,14 +6,25 @@
 /*   By: fcamoin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 20:53:58 by fcamoin           #+#    #+#             */
-/*   Updated: 2023/07/09 09:41:59 by fcamoin          ###   ########.fr       */
+/*   Updated: 2023/07/09 15:31:04 by fcamoin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include<stdio.h>
+#include<unistd.h>
+
 void	ft_putstr(char *str)
 {
-	int	i = 0;
+	int	i;
+
+	i = 0 ;
 	while (str[i])
 		i++;
 	write(1, str, i);
 }
+
+/*int	main(void)
+{
+	ft_putstr("Tu es en train de me corriger\n");
+	return (0);
+}*/
